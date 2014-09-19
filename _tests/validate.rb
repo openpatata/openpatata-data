5f5359f4606b9bf5b630e55d025719c0911ae5aa
wfdd <erdbanfed@gmail.com>
2014-09-19 22:17:41 +0300

    Revert "print filename only once, indent errors"
    This reverts commit 47d7b6dd09ba50e9e5638891690ec9c2c60e5363.

--- _tests/validate.rb
+++ _tests/validate.rb
@@ -1,22 +1,23 @@
 
-require "kwalify"
-require "yaml"
+require 'kwalify'
+require 'yaml'
 
-def validate(kind)
+def validate_documents_of_kind(kind)
   validator = Kwalify::Validator.new(
-                YAML.load_file(File.join("_tests", "schemas",
+                YAML.load_file(File.join('_tests', 'schemas',
                                          "#{kind.chop}.yaml")))
 
-  Dir[File.join(kind, "*.yaml")].each do |filename|
+  Dir[File.join(kind, '*.yaml')].each do |filename|
     errors = validator.validate(YAML.load_file(filename))
     if errors && !errors.empty?
-      puts filename
-      errors.each { |error| puts "\t[#{error.path}] #{error.message}" }
+      for error in errors
+        puts filename, "[#{error.path}] #{error.message}"
+      end
     end
   end
 end
 
-validate "bills"
-validate "committee_reports"
-validate "mps"
-validate "plenary_sittings"
+validate_documents_of_kind('bills')
+validate_documents_of_kind('committee_reports')
+validate_documents_of_kind('mps')
+validate_documents_of_kind('plenary_sittings')
